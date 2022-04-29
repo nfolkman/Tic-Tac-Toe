ee03a19b73244c12cf0c66ea86461575cf0747ab
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-04-29 16:52:00 -0500

    Add files via upload

--- Tic-Tac-Toe/main.js
+++ Tic-Tac-Toe/main.js
@@ -1,18 +1,19 @@
 const blocks = Array.from(document.querySelectorAll('.block'));
 const resetButton = document.getElementById('resetButton');
+const playerDisplay = document.querySelector('.display-player');
+const announcer = document.querySelector('.announcer');
 
 let board = ['','','','','','','','',''];
 let currentPlayer = 'X';
 let isGameActive = true;
 
 
-// 
-class Results {
-   constructor(X,O,tie) {
-      this.xWin = X;
-      this.oWin = O;
-      this.tie = tie;
-   }
+
+// this object represents the potential results of the game
+let results = {
+   xWin : 'PLAYERX_WON',
+   oWin : 'PLAYERO_WON',
+   tie : 'TIE',
 }
 
 // Array of basic winning solutions
@@ -50,3 +51,80 @@ const changePlayer = () => {
    playerDisplay.innerText = currentPlayer;
    playerDisplay.classList.add(`player${currentPlayer}`);
 }
+
+// this function takes the end game type and announces the result in the default hidden announcer section
+const announce = (type) => {
+   switch(type){
+      case results.xWin:
+         announcer.innerHTML = 'Player <span class="playerX">X</span> Wins!';
+         break;
+      case results.oWin:
+         announcer.innerHTML = 'Player <span class="playerO">O</span> Wins!';
+         break;
+      case results.tie:
+         announcer.innerHTML = 'It\'s a Tie!';
+   }
+   announcer.classList.remove('hide');
+};
+
+// this function analyzes the spots on the board to evaluate whether or not the conditions for a finished game are met (i.e.X,O,Tie). One they are met, the game cues the announced result function and the game is no longer active
+function handleResultValidation() {
+   let roundWon =false;
+   for (let i =0; i <=7; i++) {
+      const winCondition = winningConditions[i];
+      const a = board[winCondition[0]];
+      const b = board[winCondition[1]];
+      const c = board[winCondition[2]];
+      if (a=== '' || b === '' || c=== '') {
+         continue;
+      }
+      if (a===b && b===c) {
+         roundWon = true;
+         break;
+      }
+   }
+
+   if(roundWon) {
+      announce(currentPlayer === 'X' ? results.xWin : results.oWin);
+      isGameActive = false;
+      return;
+   }
+   if(!board.includes('')) announce(tie);
+}
+
+// this function evaluates user actions on the board and calls the necessary functions to switch turns/finish game
+const userAction = (block,index) => {
+   if(isValidAction(block) && isGameActive) {
+      block.innerText = currentPlayer;
+      block.classList.add(`player${currentPlayer}`);
+      updateBoard(index);
+      handleResultValidation();
+      changePlayer();
+   }
+};
+
+// this function loops through the blocks array and adds eventlisteners to retrieve user input
+blocks.forEach( (block,index) => {
+   block.addEventListener('click', () => userAction(block,index) );
+});
+
+
+// this function resets game, including the board, inner text, and makes sure X is always the starting player
+const resetBoard = () => {
+   board = ['','','','','','','','',''];
+   isGameActive = true;
+   announcer.classList.add('hide');
+
+   if(currentPlayer === 'O') {
+      changePlayer();
+   }
+
+   blocks.forEach(block => {
+      block.innerText = '';
+      block.classList.remove('playerX');
+      block.classList.remove('playerO');
+   });
+}
+
+
+resetButton.addEventListener('click', resetBoard);
